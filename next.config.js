6129a579f0840c9c9fc65ac67ab5f1864009f05e
uguremirmustafa <uguremirmustafa@gmail.com>
2021-01-10 23:02:11 +0300

    initial

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   env: {
-    BASE_URL: process.env.BASE_URL,
+    BASE_URL: 'https://mongo-nextjs-authenticated-starter.vercel.app/',
     MONGODB_URL: process.env.MONGODB_URL,
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
